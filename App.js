5958068a9018edf217caa4f7800b93d777587132
Asmita Gauri <asmitagaurikhan@gmail.com>
2021-03-30 01:43:29 +0530

    Flat List Component

--- App.js
+++ App.js
@@ -1,37 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,ScrollView } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 
 // functional component
 export default function App() {
   
  const [people,setPeople]=useState([
-   {name:"A",key:1},
-   {name:"B",key:2},
-   {name:'C',key:3},
-   {name:"D",key:4},
-   {name:"B",key:5},
-   {name:'C',key:6},
-   {name:"D",key:7},
-   {name:"B",key:8},
-   {name:'C',key:9},
-   {name:"D",key:10},
+   {name:"A",id:1},
+   {name:"B",id:2},
+   {name:'C',id:3},
+   {name:"D",id:4},
+   {name:"B",id:5},
+   {name:'C',id:6},
+   {name:"D",id:7},
+   {name:"B",id:8},
+   {name:'C',id:9},
+   {name:"D",id:10},
  ]);
   
   return (
     <View style={styles.container}>
-      {/* Look whatever is inside scroll view ,user must be able to scroll */}
-      <ScrollView>
-      {
-          people.map((item)=>{
-            return(
-                <View key={item.key}>
-                  <Text style={styles.item}>{item.name}</Text>
-                </View>
-            )
-          })
-        }
-      </ScrollView>
+      {/*  in renderItem we need to destructure item*/}
+      {/* Here we dont need to specify key its already added */}
+      {/* By default it looks for key attribute and adds it as a key, but what if we have some other attribute instead
+      like id, which is usually the case with databases, we need to explicitly tell falt list that used this as the key instead */}
+{/* One benfit of flat list over scroll view is that whenever we refresh in falt list only the components that are visible are refreshed 
+wheereas in scroll view all 100 components if present will be refreshed*/}
+      <FlatList
+      keyExtractor={(item)=> item.id}
+      data={people}
+      renderItem={({item})=>(
+        <Text style={styles.item}>{item.name}</Text>
+      )}
+      />
         
     </View>
   );
